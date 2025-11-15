3ef2e8f38d8adc13f98cfba11332c7b6647b542d
895173 <sandeepbhoomannavar455@gmail.com>
2025-11-15 12:22:48 +0530

    Create employe.py

---
+++ employe.py
@@ -0,0 +1,8 @@
+empname = "raju"
+empusn = "120"
+empsalary = "20000"
+empaddres ="hubli"
+print("employee name is",empname)
+print("employe Id is",empusn)
+print("employee salary is",empsalary)
+print("employee address is",empaddres)
